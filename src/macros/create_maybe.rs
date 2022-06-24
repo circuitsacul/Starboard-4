ec2b865f6fce997e9f9314c32a57828cf80da5fb
Circuit <circuitsacul@icloud.com>
2022-06-23 21:00:48 -0400

    remove another indent level from create_maybe!

--- src/macros/create_maybe.rs
+++ src/macros/create_maybe.rs
@@ -1,20 +1,18 @@
 // Attempt to create & return a model, otherwise return None
 #[macro_export]
 macro_rules! create_maybe {
-    ($model: ty, $($value: expr),*) => {
-        {
-            use sqlx::Error;
-            match <$model>::create($($value,)*).await {
-                Ok(value) => { Ok(Some(value)) },
-                Err(Error::Database(db_err)) => match db_err.code() {
-                    Some(code) => match &*code {
-                        "23505" => Ok(None),
-                        _ => Err(Error::Database(db_err)),
-                    },
+    ($model: ty, $($value: expr),*) => {{
+        use sqlx::Error;
+        match <$model>::create($($value,)*).await {
+            Ok(value) => { Ok(Some(value)) },
+            Err(Error::Database(db_err)) => match db_err.code() {
+                Some(code) => match &*code {
+                    "23505" => Ok(None),
                     _ => Err(Error::Database(db_err)),
                 },
-                Err(other) => Err(other),
-            }
+                _ => Err(Error::Database(db_err)),
+            },
+            Err(other) => Err(other),
         }
-    };
+    }};
 }
