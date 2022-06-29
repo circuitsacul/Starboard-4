d096fb37345a4876b89edfef407b4c543dde98a7
Circuit <circuitsacul@icloud.com>
2022-06-29 16:48:23 -0400

    request guild chunks on startup

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use twilight_gateway::Event;
+use twilight_model::gateway::payload::outgoing::RequestGuildMembers;
 
 use crate::client::bot::StarboardBot;
 use crate::core;
@@ -28,17 +29,26 @@ async fn internal_handle_event(shard_id: u64, event: Event, bot: Arc<StarboardBo
 
 async fn match_events(shard_id: u64, event: Event, bot: Arc<StarboardBot>) -> anyhow::Result<()> {
     match event {
-        Event::InteractionCreate(int) => handle_interaction(shard_id, int.0, bot).await,
-        Event::ShardConnected(event) => Ok(println!("Shard {} connected.", event.shard_id)),
+        Event::InteractionCreate(int) => handle_interaction(shard_id, int.0, bot).await?,
+        Event::ShardConnected(event) => println!("Shard {} connected.", event.shard_id),
         Event::Ready(info) => {
             if bot.application.read().await.is_none() {
                 bot.application.write().await.replace(info.application);
-                post_commands(bot).await
-            } else {
-                Ok(())
+                post_commands(bot).await?;
             }
         }
-        Event::MessageCreate(event) => core::autostar::handle(bot, event).await,
-        _ => Ok(()),
+        Event::MessageCreate(event) => core::autostar::handle(bot, event).await?,
+        Event::GuildCreate(event) => {
+            // Request members chunk
+            bot.cluster
+                .command(
+                    shard_id,
+                    &RequestGuildMembers::builder(event.id).query("", None),
+                )
+                .await?;
+        }
+        _ => {}
     }
+
+    Ok(())
 }
