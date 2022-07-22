95d999b9e315c50a548ab4d22dce533c99cc3e06
CircuitSacul <circuitsacul@icloud.com>
2022-07-22 11:36:02 -0400

    Backbone for Embed Builder (#17)
    * backbone for embedder, `star embed` cmd
    
    * remove unused field from CachedMessage

---
+++ src/core/embedder/main.rs
@@ -0,0 +1,26 @@
+use twilight_model::id::Id;
+
+use crate::client::bot::StarboardBot;
+
+use super::Embedder;
+
+impl Embedder {
+    pub fn get_top_text(&self) -> String {
+        "Hello".to_string()
+    }
+
+    pub async fn send(
+        &self,
+        bot: &StarboardBot,
+    ) -> Result<twilight_http::Response<twilight_model::channel::Message>, twilight_http::Error>
+    {
+        bot.http
+            .create_message(Id::new(
+                self.config.starboard.channel_id.try_into().unwrap(),
+            ))
+            .content(&self.get_top_text())
+            .unwrap()
+            .exec()
+            .await
+    }
+}
---
+++ src/owner/commands/embed_test.rs
@@ -0,0 +1,28 @@
+use twilight_model::gateway::payload::incoming::MessageCreate;
+
+use crate::{
+    client::bot::StarboardBot,
+    core::{embedder::Embedder, starboard::config::StarboardConfig},
+    database::Starboard,
+    unwrap_id,
+};
+
+pub async fn test_starboard_embed(bot: &StarboardBot, event: &MessageCreate) -> anyhow::Result<()> {
+    let _target = match &event.referenced_message {
+        None => anyhow::bail!("No referenced messsage"),
+        Some(target) => target,
+    };
+    let name = match event.content.strip_prefix("star embed ") {
+        None => anyhow::bail!("Invalid starboard name"),
+        Some(name) => name,
+    };
+    let sb = Starboard::get_by_name(&bot.pool, name, unwrap_id!(event.guild_id.unwrap()))
+        .await?
+        .unwrap();
+    let config = StarboardConfig::new(sb, Vec::new())?;
+
+    let e = Embedder { points: 1, config };
+    e.send(bot).await?;
+
+    Ok(())
+}
