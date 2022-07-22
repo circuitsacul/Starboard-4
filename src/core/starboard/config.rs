568370dd552b0ae7f837bf83be6ddb722e509bc0
CircuitSacul <circuitsacul@icloud.com>
2022-07-22 16:56:32 -0400

    add list_for_channel to StarboardConfig

--- src/core/starboard/config.rs
+++ src/core/starboard/config.rs
@@ -1,6 +1,15 @@
-use crate::database::{
-    helpers::settings::overrides::call_with_override_settings, Starboard, StarboardOverride,
-    StarboardSettings,
+use twilight_model::id::{
+    marker::{ChannelMarker, GuildMarker},
+    Id,
+};
+
+use crate::{
+    client::bot::StarboardBot,
+    database::{
+        helpers::settings::overrides::call_with_override_settings, Starboard, StarboardOverride,
+        StarboardSettings,
+    },
+    unwrap_id,
 };
 
 #[derive(Debug)]
@@ -38,5 +47,41 @@ impl StarboardConfig {
             overrides,
             resolved: settings,
         })
+    }
+
+    pub async fn list_for_channel(
+        bot: &StarboardBot,
+        guild_id: Id<GuildMarker>,
+        channel_id: Id<ChannelMarker>,
+    ) -> Result<Vec<Self>, SqlxOrSerdeError> {
+        let starboards = Starboard::list_by_guild(&bot.pool, unwrap_id!(guild_id)).await?;
+        let mut configs = Vec::new();
+
+        let channel_id = unwrap_id!(channel_id);
+        for sb in starboards.into_iter() {
+            let overrides =
+                StarboardOverride::list_by_starboard_and_channel(&bot.pool, sb.id, channel_id)
+                    .await?;
+            configs.push(Self::new(sb, overrides)?);
+        }
+
+        Ok(configs)
+    }
+}
+
+pub enum SqlxOrSerdeError {
+    Sqlx(sqlx::Error),
+    Serde(serde_json::Error),
+}
+
+impl From<sqlx::Error> for SqlxOrSerdeError {
+    fn from(err: sqlx::Error) -> Self {
+        Self::Sqlx(err)
+    }
+}
+
+impl From<serde_json::Error> for SqlxOrSerdeError {
+    fn from(err: serde_json::Error) -> Self {
+        Self::Serde(err)
     }
 }
