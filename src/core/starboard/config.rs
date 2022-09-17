741deb425696793bab2fe0f58f17f400d6f733e4
Maneren <49210777+Maneren@users.noreply.github.com>
2022-09-17 11:36:08 +0200

    revert if let configs

--- src/core/starboard/config.rs
+++ src/core/starboard/config.rs
@@ -93,7 +93,7 @@ impl StarboardConfig {
                 emojis.extend(sb.settings.downvote_emojis);
 
                 let configs = StarboardOverride::list_by_starboard(&bot.pool, sb.id).await?;
-                if let Some(c) = configs {
+                for c in configs {
                     let ov = c.get_overrides()?;
                     if let Some(upvote_emojis) = ov.upvote_emojis {
                         emojis.extend(upvote_emojis);
