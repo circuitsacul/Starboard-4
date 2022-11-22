81bf3fafea27f5587f5d9ad791890873cd7db7f8
CircuitSacul <circuitsacul@gmail.com>
2022-11-21 21:40:39 -0500

    use thiserror

--- src/errors.rs
+++ src/errors.rs
@@ -1,48 +1,15 @@
-use std::{error::Error, fmt::Display};
+use thiserror::Error;
 
 pub type StarboardResult<T> = Result<T, StarboardError>;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum StarboardError {
-    Sqlx(sqlx::Error),
-    Serde(serde_json::Error),
-    TwilightHttp(twilight_http::Error),
-    Reqwest(reqwest::Error),
-}
-
-impl Display for StarboardError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Sqlx(err) => err.fmt(f),
-            Self::Serde(err) => err.fmt(f),
-            Self::TwilightHttp(err) => err.fmt(f),
-            Self::Reqwest(err) => err.fmt(f),
-        }
-    }
-}
-
-impl Error for StarboardError {}
-
-impl From<sqlx::Error> for StarboardError {
-    fn from(err: sqlx::Error) -> Self {
-        Self::Sqlx(err)
-    }
-}
-
-impl From<serde_json::Error> for StarboardError {
-    fn from(err: serde_json::Error) -> Self {
-        Self::Serde(err)
-    }
-}
-
-impl From<twilight_http::Error> for StarboardError {
-    fn from(err: twilight_http::Error) -> Self {
-        Self::TwilightHttp(err)
-    }
-}
-
-impl From<reqwest::Error> for StarboardError {
-    fn from(err: reqwest::Error) -> Self {
-        Self::Reqwest(err)
-    }
+    #[error(transparent)]
+    Sqlx(#[from] sqlx::Error),
+    #[error(transparent)]
+    Serde(#[from] serde_json::Error),
+    #[error(transparent)]
+    TwilightHttp(#[from] twilight_http::Error),
+    #[error(transparent)]
+    Reqwest(#[from] reqwest::Error),
 }
