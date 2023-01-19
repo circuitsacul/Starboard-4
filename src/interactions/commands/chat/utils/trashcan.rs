f5a72e90a510d698e34f123f4fcc108fb8cd9868
Maneren <49210777+Maneren@users.noreply.github.com>
2023-01-19 05:14:41 +0100

    Refactor chunks using iterators (#152)
    * refactor loops into iterators
    
    * remove unneeded destructuring
    
    Co-authored-by: CircuitSacul <circuitsacul@icloud.com>

--- src/interactions/commands/chat/utils/trashcan.rs
+++ src/interactions/commands/chat/utils/trashcan.rs
@@ -24,30 +24,22 @@ impl TrashCan {
             return Ok(());
         }
 
-        let mut pages = Vec::new();
-        let mut curr_page = String::new();
+        let pages = trashed.chunks(50).map(|chunk| {
+            chunk
+                .iter()
+                .map(|msg| {
+                    let reason = msg.trash_reason.as_deref().unwrap_or("No reason given.");
+                    let link = fmt_message_link(guild_id, msg.channel_id, msg.message_id);
 
-        for (idx, item) in trashed.into_iter().enumerate() {
-            if idx % 50 == 0 && idx != 0 {
-                pages.push(curr_page);
-                curr_page = String::new();
-            }
-
-            let link = fmt_message_link(guild_id, item.channel_id, item.message_id);
-            curr_page.push_str(&format!(
-                "[{}]({})\n",
-                item.trash_reason
-                    .unwrap_or_else(|| "No reason given.".to_string()),
-                link,
-            ));
-        }
-
-        pages.push(curr_page);
+                    format!("[{reason}]({link})\n")
+                })
+                .collect::<String>()
+        });
 
         let author_id = ctx.interaction.author_id().unwrap();
         paginator::simple(
             &mut ctx,
-            pages.into_iter().map(|page| (Some(page), None)).collect(),
+            pages.map(|page| (Some(page), None)).collect(),
             author_id,
             true,
         )
