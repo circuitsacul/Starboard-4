30e0a564cd68167fc294268e92d2ce51832caf7c
CircuitSacul <circuitsacul@icloud.com>
2023-01-24 02:51:10 -0500

    fix the stats command

--- src/interactions/commands/chat/botstats.rs
+++ src/interactions/commands/chat/botstats.rs
@@ -1,5 +1,6 @@
 use thousands::Separable;
 use twilight_interactions::command::{CommandModel, CreateCommand};
+use twilight_util::builder::embed::EmbedFieldBuilder;
 
 use crate::{errors::StarboardResult, interactions::context::CommandCtx, utils::embed};
 
@@ -10,14 +11,19 @@ pub struct BotStats;
 impl BotStats {
     pub async fn callback(self, mut ctx: CommandCtx) -> StarboardResult<()> {
         let guilds = ctx.bot.cache.guilds.len();
-        let users = ctx.bot.cache.users.len();
+        let cached_users = ctx.bot.cache.users.len();
+        let cached_messages = ctx.bot.cache.messages.len();
 
         let emb = embed::build()
             .title("Starboard's Stats")
-            .description(format!(
-                "Servers: {}\nUsers: {}",
-                guilds.separate_with_commas(),
-                users.separate_with_commas()
+            .description(format!("Total Servers: {}", guilds.separate_with_commas(),))
+            .field(EmbedFieldBuilder::new(
+                "Cache",
+                format!(
+                    "Cached Users: {}\nCached Messages: {}",
+                    cached_users.separate_with_spaces(),
+                    cached_messages.separate_with_commas(),
+                ),
             ))
             .build();
         ctx.respond(ctx.build_resp().embeds([emb]).build()).await?;
