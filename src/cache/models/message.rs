7ded1eaecbe6b605964618cc0a1b238b76c4a335
CircuitSacul <circuitsacul@icloud.com>
2023-03-22 17:04:17 -0400

    handle deleted webhooks

--- src/cache/models/message.rs
+++ src/cache/models/message.rs
@@ -7,12 +7,39 @@ use twilight_model::{
         marker::{MessageMarker, UserMarker},
         Id,
     },
+    user::User,
+    util::ImageHash,
 };
 
 use crate::utils::system_content::SystemContent;
 
+#[derive(Clone)]
+pub struct CachedMessageAuthor {
+    pub name: String,
+    pub avatar: Option<ImageHash>,
+}
+
+impl From<User> for CachedMessageAuthor {
+    fn from(user: User) -> Self {
+        Self {
+            name: user.name,
+            avatar: user.avatar,
+        }
+    }
+}
+
+impl From<&User> for CachedMessageAuthor {
+    fn from(user: &User) -> Self {
+        Self {
+            name: user.name.clone(),
+            avatar: user.avatar,
+        }
+    }
+}
+
 pub struct CachedMessage {
     pub author_id: Id<UserMarker>,
+    pub author: CachedMessageAuthor,
     pub content: String,
     pub attachments: Vec<Attachment>,
     pub stickers: Vec<MessageSticker>,
@@ -25,6 +52,7 @@ impl From<Message> for CachedMessage {
         let content = msg.system_content();
         Self {
             author_id: msg.author.id,
+            author: msg.author.into(),
             attachments: msg.attachments,
             embeds: msg.embeds,
             content,
@@ -38,6 +66,7 @@ impl From<&Message> for CachedMessage {
     fn from(msg: &Message) -> Self {
         Self {
             author_id: msg.author.id,
+            author: (&msg.author).into(),
             attachments: msg.attachments.clone(),
             embeds: msg.embeds.clone(),
             content: msg.system_content(),
