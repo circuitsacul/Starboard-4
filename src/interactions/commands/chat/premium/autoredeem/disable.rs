ed889602ce37a446239712b734e8364919d7470d
CircuitSacul <circuitsacul@icloud.com>
2023-07-02 20:17:05 -0400

    auto-redeem i18n

--- src/interactions/commands/chat/premium/autoredeem/disable.rs
+++ src/interactions/commands/chat/premium/autoredeem/disable.rs
@@ -1,27 +1,38 @@
 use twilight_interactions::command::{CommandModel, CreateCommand};
 
 use crate::{
-    database::DbMember, errors::StarboardResult, interactions::context::CommandCtx,
+    database::DbMember, errors::StarboardResult, interactions::context::CommandCtx, locale_func,
     utils::id_as_i64::GetI64,
 };
 
+locale_func!(autoredeem_disable);
+locale_func!(autoredeem_disable_option_server);
+
 #[derive(CommandModel, CreateCommand)]
-#[command(name = "disable", desc = "Disable autoredeem for a server.")]
+#[command(
+    name = "disable",
+    desc = "Disable autoredeem for a server.",
+    desc_localizations = "autoredeem_disable"
+)]
 pub struct Disable {
     /// The server to disable autoredeem for.
-    #[command(autocomplete = true)]
+    #[command(
+        autocomplete = true,
+        desc_localizations = "autoredeem_disable_option_server"
+    )]
     server: Option<String>,
 }
 
 impl Disable {
     pub async fn callback(self, mut ctx: CommandCtx) -> StarboardResult<()> {
         let user_id = ctx.interaction.author_id().unwrap().get_i64();
+        let lang = ctx.user_lang();
 
         let guild_id = 'out: {
             let Some(input_guild) = self.server else {
                 let Some(guild_id) = ctx.interaction.guild_id else {
                     ctx.respond_str(
-                        "Please specify a server, or run this command inside one.",
+                        lang.autoredeem_disable_invalid_guild(),
                         true
                     ).await?;
                     return Ok(());
@@ -32,7 +43,7 @@ impl Disable {
 
             let Ok(guild_id) = input_guild.parse::<i64>() else {
                 ctx.respond_str(
-                    "Please entire a server ID, or select a server from the options.", 
+                    lang.autoredeem_disable_invalid_guild(),
                     true
                 ).await?;
                 return Ok(());
@@ -43,7 +54,8 @@ impl Disable {
 
         DbMember::set_autoredeem_enabled(&ctx.bot.pool, user_id, guild_id, false).await?;
 
-        ctx.respond_str("Autoredeem disabled.", true).await?;
+        ctx.respond_str(lang.autoredeem_disable_done(), true)
+            .await?;
 
         Ok(())
     }
