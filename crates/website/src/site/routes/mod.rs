20346a9a5170ceced64ce9151d6997ff2d54c802
CircuitSacul <circuitsacul@icloud.com>
2023-08-03 18:42:52 -0400

    outsource route definitions

--- crates/website/src/site/routes/mod.rs
+++ crates/website/src/site/routes/mod.rs
@@ -11,31 +11,43 @@ pub fn Index(cx: Scope) -> impl IntoView {
     view! { cx,
         <Router>
             <Routes>
-                <Route path="" view=website::Website>
-                    <main>
-                        <Route path="" view=website::home::Home/>
-
-                        <Route path="/*any" view=errors::not_found::NotFound/>
-                    </main>
-                </Route>
-
-                <Route path="/dashboard" view=dashboard::Dashboard>
-                    <main>
-                        <Route path="" view=dashboard::server_list::ServerList/>
-                        // <Route path="/server/:id" view=dashboard::server::Server>
-                        //     <Route path="" view=dashboard::server::overview::Overview/>
-                        //     <Route path="/starboards" view=dashboard::starboards::Starboards>
-                        //         <Route path=":id" view=dashboard::starboards::Requirements/>
-                        //         <Route path=":id/behavior" view=dashboard::starboards::Behavior/>
-                        //         // ...other setting categories
-                        //         // requirements, behavior, embed, style, regex, filters
-                        //     </Route>
-                        // </Route>
-
-                        <Route path="/*any" view=errors::not_found::NotFound/>
-                    </main>
-                </Route>
+                <WebsiteRoutes/>
+                <DashboardRoutes/>
             </Routes>
         </Router>
+    }
+}
+
+#[component(transparent)]
+fn WebsiteRoutes(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <Route path="" view=website::Website>
+            <main>
+                <Route path="" view=website::home::Home/>
+
+                <Route path="/*any" view=errors::not_found::NotFound/>
+            </main>
+        </Route>
+    }
+}
+
+#[component(transparent)]
+fn DashboardRoutes(cx: Scope) -> impl IntoView {
+    view! { cx,
+        <Route path="/dashboard" view=dashboard::Dashboard>
+            <main>
+                <Route path="" view=dashboard::server_list::ServerList/>
+                // <Route path="/server/:id" view=dashboard::server::Server>
+                //     <Route path="" view=dashboard::server::overview::Overview/>
+                //     <Route path="/starboards" view=dashboard::starboards::Starboards>
+                //         <Route path=":id" view=dashboard::starboards::Requirements/>
+                //         <Route path=":id/behavior" view=dashboard::starboards::Behavior/>
+
+                //     </Route>
+                // </Route>
+
+                <Route path="/*any" view=errors::not_found::NotFound/>
+            </main>
+        </Route>
     }
 }
