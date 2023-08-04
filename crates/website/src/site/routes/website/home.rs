cf161d8c32437b653e7f9a78fc3fe99b78997bda
CircuitSacul <circuitsacul@icloud.com>
2023-08-03 21:56:22 -0400

    add hero to Home

--- crates/website/src/site/routes/website/home.rs
+++ crates/website/src/site/routes/website/home.rs
@@ -2,14 +2,9 @@ use leptos::*;
 
 #[component]
 pub fn Home(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
-
     view! { cx,
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click class="btn">
-            "Click Me: "
-            {count}
-        </button>
+        <div class="hero">
+            Hello 2!
+        </div>
     }
 }
